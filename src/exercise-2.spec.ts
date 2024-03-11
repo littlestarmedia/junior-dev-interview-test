722bf91a0f743b0d444c5bb95fa3efdc7605a52b
Adam Lewis <adam.lewis@littlestarmedia.com>
2024-03-11 16:43:24 +0000

    Initial commit.

---
+++ src/exercise-2.spec.ts
@@ -0,0 +1,7 @@
+import { mergeSortedArrays } from './exercise-2';
+
+test('merges two sorted arrays correctly', () => {
+    const array1 = [1, 3, 5];
+    const array2 = [2, 4, 6];
+    expect(mergeSortedArrays(array1, array2)).toEqual([1, 2, 3, 4, 5, 6]);
+});
---
+++ src/exercise-2.ts
@@ -0,0 +1,1 @@
+export function mergeSortedArrays(): [] {}
